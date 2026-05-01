e20c3a806b77ed791b7b646ce34b9dc3db5215c5
CarlosBavon <carlosbavon46@gmail.com>
2026-05-02 00:35:18 +0300

    Debug

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API = axios.create({ baseURL: 'https://stylebymk-back.onrender.com' });
+const API = axios.create({ baseURL: "https://stylebymk-back.onrender.com/api" });
 
-export const createBooking = (data) => API.post('/bookings', data);
-export const createEnquiry = (data) => API.post('/enquiries', data);
-export const createContact = (data) => API.post('/contact', data);
+export const createBooking = (data) => API.post("/bookings", data);
+export const createEnquiry = (data) => API.post("/enquiries", data);
+export const createContact = (data) => API.post("/contact", data);
 
 export default API;
