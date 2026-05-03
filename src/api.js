87aa952134f1e3a70cbbdd17ccde28860845f748
CarlosBavon <carlosbavon46@gmail.com>
2026-05-03 22:57:30 +0300

    Adding backend

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
-});
+const API = axios.create({ baseURL: "https://stylebymk-back.onrender.com/api" });
 
 export const createBooking = (data) => API.post("/bookings", data);
 export const createEnquiry = (data) => API.post("/enquiries", data);
