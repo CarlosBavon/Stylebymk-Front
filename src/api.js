fa1bfda4deb3ff1846c111fd549d209fefbfb3c8
CarlosBavon <carlosbavon46@gmail.com>
2026-05-31 13:19:23 +0300

    Adjusting logo size

--- src/api.js
+++ src/api.js
@@ -5,5 +5,6 @@ const API = axios.create({ baseURL: "https://stylebymk-back.onrender.com/api" })
 export const createBooking = (data) => API.post("/bookings", data);
 export const createEnquiry = (data) => API.post("/enquiries", data);
 export const createContact = (data) => API.post("/contact", data);
+export const createContact = (data) => API.post("/testimonials", data);
 
 export default API;
